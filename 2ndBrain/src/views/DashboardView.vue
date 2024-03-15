671de0d3ae8cee38e21d0c40b0e09aab311785b6
JPNx1 <zimmermann.marlon.mzi@gmail.com>
2024-03-15 14:14:03 +0100

    setup for dashboard and route

---
+++ 2ndBrain/src/views/DashboardView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import DashboardItem from '../components/DashboardItem.vue'
+</script>
+
+<template>
+    <main>
+        <div class="dashboard">
+        <h1>aaaaaa</h1>
+        <DashboardItem />
+
+        </div>
+    </main>
+</template>
